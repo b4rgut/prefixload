17404c5828e422db0b1f2c0e6f7e1c5a6449b14f
Aleksey Kalsin <aleksey@kalsin.pro>
2025-07-19 23:52:01 +0300

    fix: delete an extra newline in the message output

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ async fn main() {
     let cli = Cli::parse();
 
     match cli.run().await {
-        Ok(result) => println!("{}", result),
+        Ok(result) => print!("{}", result),
         Err(err) => {
             eprintln!("{}", err);
             std::process::exit(1);
