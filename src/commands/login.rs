bf1120458aa9dcf1e1c47d6057d0258289ea82ea
Aleksey Kalsin <aleksey@kalsin.pro>
2025-07-23 21:05:58 +0300

    feat(commands/login): checking the availability of a bucket

--- src/commands/login.rs
+++ src/commands/login.rs
@@ -1,3 +1,5 @@
+use crate::clients::s3::{S3Client, S3ClientOptions};
+use crate::config::Config;
 use crate::error::Result;
 use requestty::Question;
 
@@ -23,6 +25,21 @@ fn input_credentials() -> Result<(String, String)> {
     Ok((access_key, secret_key))
 }
 
+async fn credentials_valid(access_key: &str, secret_key: &str) -> Result<bool> {
+    let config: Config = Config::load()?;
+
+    let s3_options = S3ClientOptions::default()
+        .with_access_key(access_key.to_string())
+        .with_secret_key(secret_key.to_string())
+        .with_endpoint(config.endpoint.clone());
+
+    let s3_client = S3Client::new(s3_options).await.unwrap();
+
+    let result = s3_client.check_bucket_access(&config.bucket).await;
+
+    Ok(result.is_ok())
+}
+
 fn store_credentials(access_key: &str, secret_key: &str) -> Result<()> {
     println!(
         "Storing:\n  access = {}\n  secret = {}",
@@ -34,7 +51,10 @@ fn store_credentials(access_key: &str, secret_key: &str) -> Result<()> {
 pub async fn run() -> Result<String> {
     let (access_key, secret_key) = input_credentials()?;
 
-    store_credentials(&access_key, &secret_key)?;
+    if credentials_valid(&access_key, &secret_key).await? {
+        store_credentials(&access_key, &secret_key)?;
+        return Ok("Credentials have been saved successfully!".to_string());
+    }
 
-    Ok("Credentials not stored".to_string())
+    Ok("Credentials not valid".to_string())
 }
